b9e6993012a780201a6cf88bc6789a5076ec6cf9
EnricoValente <enricovalente42@gmail.com>
2023-06-09 15:57:41 +0200

    Completato esercizio

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+
+const name = prompt ('Inserisci nome')
+
+console.log (name, typeof name)
+
+
+
+const surname = prompt ('Inserisci cognome')
+
+console.log (surname, typeof surname)
+
+
+const age = prompt ('Inserisci età')
+
+console.log (age, typeof age)
+
+const color = prompt ('il tuo colore preferto')
+
+console.log (color, typeof color)
+
+const classNumber = prompt ('classe')
+
+console.log (classNumber, typeof classNumber)
+
+document.addElementById('my-data').innerHTML = name + ' ' + surname + ' ' + age + ' ' + color + ' ' + classNumber
+
